841deae5fd5edf47030ddd3616200b18f4998743
ryoheiTakano <jooj10.27@icloud.com>
2023-11-30 23:22:49 +0900

    CRUD Cのコピー作成

--- app/src/main/java/com/example/demo/controllers/RootController.java
+++ app/src/main/java/com/example/demo/controllers/RootController.java
@@ -9,22 +9,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.demo.models.CreatForm;
 import com.example.demo.models.InquiryForm;
+import com.example.demo.repositries.CreatRepository;
 import com.example.demo.repositries.InquiryRepository;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("/") //ドメイン以降のURLを指定（https://127.0.0.1/←「/」の部分　「//127.0.0.1」がドメイン）
 public class RootController {
 
 	@Autowired
 	InquiryRepository repository;
+	@Autowired
+	CreatRepository creatRepository;
 
-	@GetMapping
+	@GetMapping //RequestMappingで指定したvalue以降のURLを指定（何も指定していない場合「/」までのURLはこのメソッドを起動する）
 	public String index() {
 		return "root/index";
 	}
 
-	@GetMapping("/form")
+	@GetMapping("/form") //URLの「/」以降が「/form」となっている場合にこのメソッドが起動
 	public String form(InquiryForm inquiryForm) {
 		return "root/form";
 	}
@@ -40,5 +44,23 @@ public class RootController {
 		inquiryForm.clear();
 		model.addAttribute("message", "お問い合わせを受け付けました。");
 		return "root/form";
+	}
+	
+	@GetMapping("/creat")
+	public String form(CreatForm creatForm) {
+		return "root/creat";
+	}
+
+	@PostMapping("/creat")
+	public String creatForm(@Validated CreatForm creatForm, BindingResult bindingResult, Model model) {
+		if (bindingResult.hasErrors()) {
+			return "root/creat";
+		}
+
+		// RDBと連携できることを確認しておきます。
+		creatRepository.saveAndFlush(creatForm);
+		creatForm.clear();
+		model.addAttribute("message", "お問い合わせを受け付けました。");
+		return "root/creat";
 	}
 }
